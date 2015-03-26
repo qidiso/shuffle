0a3b728f192ae65cedfd11158e9ba1c2389bddb1
Alex Naser <c.a.naser@gmail.com>
2015-03-26 07:40:42 -0400

    boilerplate clm face tracking now displayed

---
+++ scripts/app.js
@@ -0,0 +1,61 @@
+var vid       = document.getElementById('mirror');
+var overlay   = document.getElementById('overlay');
+var overlayCC = overlay.getContext('2d');
+
+var ctrack    = new clm.tracker({ useWebGL : true });
+ctrack.init(pModel);
+
+stats = new Stats();
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.top = '0px';
+document.getElementById('container').appendChild( stats.domElement );
+
+function enablestart() {
+  var startbutton = document.getElementById('startbutton');
+  startbutton.value = "start";
+  startbutton.disabled = null;
+}
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.URL = window.URL || window.webkitURL || window.msURL || window.mozURL;
+// check for camerasupport
+
+if (navigator.getUserMedia) {
+  navigator.getUserMedia({ video : true }, function(stream) {
+    if (vid.mozCaptureStream) {
+      vid.mozSrcObject = stream;
+    } else {
+      vid.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+    }
+    vid.play();
+  }, function() {
+    alert("There was some problem trying to fetch video from your webcam.");
+  });
+} else {
+  alert("Your browser does not seem to support getUserMedia.");
+}
+vid.addEventListener('canplay', enablestart, false);
+
+function startVideo() {
+  // start video
+  vid.play();
+  // start tracking
+  ctrack.start(vid);
+  // start loop to draw face
+  drawLoop();
+}
+
+function drawLoop() {
+  requestAnimationFrame(drawLoop);
+  overlayCC.clearRect(0, 0, 400, 300);
+  if (ctrack.getCurrentPosition()) {
+    ctrack.draw(overlay);
+  }
+}
+
+// update stats on every iteration
+document.addEventListener('clmtrackrIteration', function(event) {
+  stats.update();
+}, false);
+
+startVideo()
